65942a153ed68e9b824c6e8354fea1bdf4ca0a32
Daniel <arutyunyan.daniel23@mail.ru>
2018-02-12 20:52:56 +0300

    add notificztion for block button while doing ajax respons

--- WebTourist/Scripts/MyScripts/AjaxRequest.js
+++ WebTourist/Scripts/MyScripts/AjaxRequest.js
@@ -8,22 +8,30 @@
         url: "/Home/EventMouseClick",
         data: JSON.stringify(routeInformation),
         contentType: "application/json; charset=utf-8",
-
+        beforeSend: function () {
+            $("#transparentBackground").show('slow');
+            $("#preloader").show('slow');
+        },
         success: function (data) {
             isEnterLocation = true;
             DrawPathToExcursionRoute(stringToArrayLatLng(data.Route));
             showFinishRouteMarker(data.finishCoordinatesLat, data.finishCoordinatesLng, map);
-            showDistanceDuration(data.Distance, data.Duration);         
-
+            showDistanceDuration(data.Distance, data.Duration);
+            
             arrayIdVisitedExcursionRoutes = [];
             arrayIdVisitedExcursionRoutes.push(data.listIdVisitedRoutes[0]);
         },
+        complete: function () {
+            $("#transparentBackground").hide('slow');
+            $("#preloader").hide('slow');
+        },
         error: errorFunc
     });
+   
 }
 
 function eventButtomClick() {
-   if (!isEnterLocation) {
+    if (!isEnterLocation) {
         alert('Select your location');
         return;
     }
@@ -35,11 +43,11 @@ function eventButtomClick() {
 
     for (var i = 0; i < arrayIdVisitedExcursionRoutes.length; i++)
         routeInformation.listIdVisitedRoutes.push(arrayIdVisitedExcursionRoutes[i]);
-    
+
     $.ajax({
         type: "POST",
         url: "/Home/EventButClickNextRoute",
-        data: JSON.stringify(routeInformation ),
+        data: JSON.stringify(routeInformation),
         contentType: "application/json; charset=utf-8",
 
         success: function (data) {
@@ -49,17 +57,25 @@ function eventButtomClick() {
 
             arrayIdVisitedExcursionRoutes = [];
             for (var i = 0; i < data.listIdVisitedRoutes.length; i++)
-            {
                 arrayIdVisitedExcursionRoutes.push(data.listIdVisitedRoutes[i]);
-            }
         },
-
         error: errorFunc
-
     });
+    
 }
 
 function errorFunc(errorData) {
     alert('Ошибка' + errorData.responseText);
 }
 
+$(document).ready(function () {
+    $('#but1').click(eventButtomClick);
+    $("#but1").mouseover(function () {
+        $(this).css("color", "#00B3FD");
+    });
+    $("#but1").mouseout(function () {
+        $(this).css("color", "#666666");
+    });
+});
+
+$(document).ready(loadPage);
